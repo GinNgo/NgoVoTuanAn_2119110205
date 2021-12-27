e3a1a7fff3142d79079fbf6cbb4e9259ec924f55
Ngô Võ Tuấn An <ngovotuananh@gmail.com>
2021-12-27 16:34:33 +0700

    add DTO

--- Cau1/EmloyeeDTO/EmloyeeDTO.cs
+++ Cau1/EmloyeeDTO/EmloyeeDTO.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Cau1.EmloyeeDTO
 {
-    class EmloyeeDTO
+    public class EmloyeeDTO
     {
+        public string IdEmployee { get; set; }
+        public string Name { get; set; }
+        public DateTime DateBirth { get; set; }
+        public bool Gender { get; set; }
+        public string PlaceBirth { get; set; }
+        public DepartmentDTO Department { get; set; }
+        public string IdDepartment { get { return Department.Name; } }
     }
 }
